f2df31fefcd8779b8324daa0fcd3b488ebeca253
Hao Chen <501018786@qq.com>
2022-07-26 13:41:03 +0800

    fix dcatenate in lab2

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -81,6 +81,9 @@ public class IntList {
      */
 
     public static IntList dcatenate(IntList A, IntList B) {
+        if (A == null) {
+            return B;
+        }
         IntList ptr = A;
         while (ptr.rest != null) {
             ptr = ptr.rest;
